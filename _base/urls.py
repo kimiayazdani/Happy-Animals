fb9a8d73a09b4f5769a3cede3cde457af1a7c4f4
NimaFathi <nimafathinimafathi@gmail.com>
2020-11-20 04:46:28 +0330

    refactor urls.py

--- _base/urls.py
+++ _base/urls.py
@@ -1,25 +1,14 @@
-
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from django.conf.urls import url, include
 from . import settings
 from rest_framework.authtoken.views import obtain_auth_token
-from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
+from django.contrib.staticfiles.urls import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls'), name='rest_framework'),
     path('api/v1/adoption/', include('adoption.urls')),
-    # path('channel/', include('channel.urls')),
     path('api/v1/account/', include('account_management.urls_v1')),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth')
-    # path('mainpage/',include('MainPage.urls')),
-    # path('notifications/', include('notifications.urls')),
-    # path('password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
-    # path('search/', include('search.urls')),
-    # path('api/auth/oauth/', include('rest_framework_social_oauth2.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-# urlpatterns += staticfiles_urlpatterns()
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
